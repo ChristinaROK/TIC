df6e65ca6b064d379be558983b3ec0dea9d6d613
Christina Shin <40985245+ChristinaROK@users.noreply.github.com>
2021-04-27 00:16:09 +0900

    Update 20210426.py

--- 20210426.py
+++ 20210426.py
@@ -8,3 +8,8 @@ def solution(numbers):
     offset = lambda x : x + x*(max_len-len(x)) if max_len>2*len(x) else x + x[:max_len-len(x)] 
     return str(int("".join(sorted(str_num, key=offset, reverse=True))))
 
+
+# 나는 n(str 숫자)를 늘리는 한도를 정하기 위해 offset이란 함수를 만들었는데 사실 굳이 그럴 필요 없이 그냥 최대로 늘려도 상관 없었음...
+def solution1(numbers):
+    str_num = [str(n) for n in numbers]
+    return str(int("".join(sorted(str_num, key=lambda x : x*3, reverse=True))))
