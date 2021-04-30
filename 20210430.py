188f3cffe11fead14c3ab03c812f40c7af27ccdc
Christina Shin <40985245+ChristinaROK@users.noreply.github.com>
2021-05-01 00:03:00 +0900

    Update 20210430.py

--- 20210430.py
+++ 20210430.py
@@ -9,3 +9,20 @@ def solution(d, b):
             break
         answer +=1
     return answer
+# Pass
+
+# Programmers 실패율, (level-1)
+
+from collections import Counter
+
+def solution(N, stages):
+    staying_users = Counter(stages)
+    staying_users = dict(sorted(staying_users.items(), key=lambda item: item[0]))
+    tot = len(stages)
+    res = {}
+    for stage, n in staying_users.items():
+        res[stage] = (n / tot)
+        tot-=n
+    return sorted([i for i in range(1,N+1)], key = lambda x : res.get(x, 0), reverse=True)
+
+# Pass
