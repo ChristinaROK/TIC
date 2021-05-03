1fd9a0a7a9d056f9e3fc21c980682b51327c3792
Christina Shin <40985245+ChristinaROK@users.noreply.github.com>
2021-05-04 00:38:17 +0900

    Update 20210503.py

--- 20210503.py
+++ 20210503.py
@@ -3,32 +3,35 @@
 def solution(s):
     ori=s
     max_len = len(ori)//2
-    answer = 0
+    answer = len(ori)
 
-    while max_len > 0: # max greedy
+    while max_len > 0: 
+        sub_answer = 0
         left_str_len = 0
-        
+
         while len(s) >0: # find v(equal cnt) for each pattern substrings
             pattern = s[:max_len]
-            
+
             idx = max_len
-            v = 0
+            v = 1
             while True:
-                if pattern == s[idx:2*idx]:
+                if pattern == s[idx:idx+max_len]:
                     v+=1
                     idx+= max_len
                 else:
                     break      
-            
-            if v > 0:
-                answer+=len(str(v))+max_len
+
+            if v > 1:
+                sub_answer+=len(str(v))+max_len
             else:
                 left_str_len += len(pattern)
             s = s[idx:]
-            
-        if answer>0:
-            return answer+left_str_len
-        else:
-            s = ori
-            max_len-=1
-    return len(ori)
+
+        if sub_answer>0:
+            answer = min(answer, sub_answer + left_str_len)
+        s = ori
+        max_len-=1
+    return answer
+
+
+ # Pass
