c7921945df50401c5218a0067d9e5aeed4c25c11
Christina Shin <40985245+ChristinaROK@users.noreply.github.com>
2021-05-12 01:12:26 +0900

    Update 20210511.py

--- 20210511.py
+++ 20210511.py
@@ -1,1 +1,31 @@
-# Programmers
+# Programmers 비밀 지도 (level-1)
+
+# def binary(x,n,res):
+#     i=n
+#     while 2**i>x:
+#         i-=1
+
+#     res.append(i)
+
+#     if x-(2**i)==0:
+#         return res
+#     return binary(x-(2**i), n, res)
+def binary(x):
+    return [i for i, s_ in enumerate(bin(x).replace('0b','')[::-1]) if s_ =="1"]
+
+def solution(n, arr1, arr2):
+    answer = []
+    for x1, x2 in zip(arr1, arr2):
+        res = set.union(set(binary(x1)), binary(x2))
+
+        answer_ = [" "]*n
+        for i in res:
+            answer_[i] = "#"
+        answer.append("".join(reversed(answer_)))
+
+    return answer
+  
+  
+  # pass
+  # binary 직접 구현하다가 run-time error나서 bin 내장 함수 씀 ㅠㅠ 
+  # 쉬운 문제를 너무 어렵게 생각했음...
