6a4346ccad5fc8fe59e8d3a74b8c760fdbcaa5cf
Christina Shin <40985245+ChristinaROK@users.noreply.github.com>
2021-05-15 00:37:16 +0900

    Update 20210514.py

--- 20210514.py
+++ 20210514.py
@@ -1,1 +1,15 @@
-# Programmers
+# Programmers 소수 만들기 (level-1)
+
+from itertools import combinations
+
+def prime(n):
+    for i in range(2,n):
+        if n%i==0:
+            return 0
+    return 1
+
+def solution(nums):
+    return sum([prime(sum(cand)) for cand in combinations(nums, 3) if sum(cand)%2==1])
+  
+  
+  # pass 
