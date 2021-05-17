d6ffffd4845c5bb6dc3f2fe69cba38172b922131
Christina Shin <40985245+ChristinaROK@users.noreply.github.com>
2021-05-18 00:22:27 +0900

    Update 20210517.py

--- 20210517.py
+++ 20210517.py
@@ -1,1 +1,21 @@
-# Programmers 
+# Programmers 주식 가격 (level2) stack queue
+
+def solution(prices):
+    answer = []
+    n_stocks = len(prices)
+    
+    while n_stocks>0:
+        p = prices.pop(0)
+        n_stocks-=1
+        i=0
+        sec=0
+        while i<n_stocks:
+            sec+=1
+            if prices[i]<p:
+                break
+            i+=1
+        answer.append(sec)
+        
+    return answer
+  
+  # 효율성 테스트 통과 못함
