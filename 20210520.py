4a987b6bef407b41b39269ba7d7106ec00548025
Christina Shin <40985245+ChristinaROK@users.noreply.github.com>
2021-05-21 00:34:53 +0900

    Update 20210520.py

--- 20210520.py
+++ 20210520.py
@@ -12,3 +12,21 @@ def solution(priorities, location):
             res.append(i)
     answer = res.index(location)+1
     return answer
+
+# pass
+
+# better version
+# 1. deque (double ended queue)는 맨 앞, 맨 뒤 삽입 삭제의 시작 복잡도가 O(1)이다. 
+# 2. location index의 출력 순서가 answer이기 때문에 res를 만들 필요 없이 rotation하지 않으면 answer에 +1하고 cur index와 location이 일치하면 반환 
+from collections import deque
+def solution(priorities, location):
+    queue = deque([(i,v) for i,v in enumerate(priorities)])
+    answer=0
+    while True:
+        cur = queue.popleft()
+        if any(cur[1]<q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer+=1
+            if location==cur[0]:
+                return answer
