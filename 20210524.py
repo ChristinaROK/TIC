95b9b73f0da703d5baa8df522425be8d7cf0d702
Christina Shin <40985245+ChristinaROK@users.noreply.github.com>
2021-05-25 00:17:56 +0900

    Update 20210524.py

--- 20210524.py
+++ 20210524.py
@@ -1,1 +1,21 @@
-# Programmers
+# Programmers 전화번호목록 level-2 hash
+
+import copy
+def solution(phone_book):
+    answer = True
+    while phone_book:
+        first_num = phone_book.pop(0)
+        first_set = {first_num}
+        length = len(first_num)
+        
+        phone_book_cp = copy.deepcopy(phone_book)
+        for num in phone_book_cp:
+            if len(num)>length:
+                break
+            first_set.add(phone_book.pop(0))
+        
+        if first_set.intersection({num[:length] for num in phone_book}):
+            answer = False
+            break
+            
+    return answer
