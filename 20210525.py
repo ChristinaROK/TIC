8ff7b656e0759a7d48a0eeeed689df1b878af74c
Christina Shin <40985245+ChristinaROK@users.noreply.github.com>
2021-05-26 02:27:29 +0900

    Update 20210525.py

--- 20210525.py
+++ 20210525.py
@@ -1,1 +1,8 @@
-#
+# Programmers 문자열 내 p와 y의 개수 (level-1) 
+
+import re
+def solution(s):
+    return len(re.findall(r'[Pp]',s)) == len(re.findall(r'[Yy]',s))
+
+# pass
+# s.lower()후 s.count("p") 할 수도 있음
